cfb0c345201865c53f1c6e6db957964a36ba2804
Giannagp <giannagp@users.noreply.github.com>
2020-10-15 13:42:11 -0600

    project one

--- web/projectone/view/joblist.php
+++ web/projectone/view/joblist.php
@@ -5,32 +5,27 @@
     <h1> Job Post </h1>
 
     <?php
-    $dbUrl = getenv('DATABASE_URL');
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
-    if (empty($dbUrl)) {
-        // example localhost configuration URL with postgres username and a database called cs313db
-        $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
-    }
+  $dbOpts = parse_url($dbUrl);
 
-    $dbopts = parse_url($dbUrl);
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-    print "<p>$dbUrl</p>\n\n";
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"], '/');
-
-    print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
-
-    try {
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    } catch (PDOException $ex) {
-        print "<p>error: $ex->getMessage() </p>\n\n";
-        die();
-    }
-
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
     $statement = $db->query('SELECT jobName, jobLocation FROM job');
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         echo 'job: ' . $row['jobName'] . ' location: ' . $row['jobLocation'] . '<br/>';
