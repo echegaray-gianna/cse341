7eaf4f2db7988c9477ebfbb982145d6d9749f694
Giannagp <giannagp@users.noreply.github.com>
2020-10-15 14:03:10 -0600

    project one

--- web/projectone/view/joblist.php
+++ web/projectone/view/joblist.php
@@ -5,52 +5,27 @@
     <h1> Job Post </h1>
 
     <?php
-function get_db() {
-	$db = NULL;
 
-	try {
-		// default Heroku Postgres configuration URL
-		$dbUrl = getenv('DATABASE_URL');
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
-		if (!isset($dbUrl) || empty($dbUrl)) {
-			// example localhost configuration URL with user: "ta_user", password: "ta_pass"
-			// and a database called "scripture_ta"
-			$dbUrl = "postgres://xwhtsbrazoztaf:ef0a2f61008b696a391e62abfd4d798920226ee5cf78ea8c4afc9595ee290455@ec2-54-158-222-248.compute-1.amazonaws.com:5432/db7dhsc98v47n7";
+  $dbOpts = parse_url($dbUrl);
 
-			// NOTE: It is not great to put this sensitive information right
-			// here in a file that gets committed to version control. It's not
-			// as bad as putting your Heroku user and password here, but still
-			// not ideal.
-			
-			// It would be better to put your local connection information
-			// into an environment variable on your local computer. That way
-			// it would work consistently regardless of whether the application
-			// were running locally or at heroku.
-		}
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-		// Get the various parts of the DB Connection from the URL
-		$dbopts = parse_url($dbUrl);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
-
-		// Create the PDO connection
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch (PDOException $ex) {
-		// If this were in production, you would not want to echo
-		// the details of the exception.
-		echo "Error connecting to DB. Details: $ex";
-		die();
-	}
-
-	return $db;
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
 
     $statement = $db->query('SELECT jobName, jobLocation FROM job');
