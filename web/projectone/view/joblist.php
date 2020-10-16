3126892aa5ecfa742d3904b5f1330b25cfda6807
Giannagp <giannagp@users.noreply.github.com>
2020-10-16 17:24:31 -0600

    project one

--- web/projectone/view/joblist.php
+++ web/projectone/view/joblist.php
@@ -55,6 +55,14 @@
             
 </label> -->
 
+
+<select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+
 </main>
 
 
