cf70ddc2b6018c20cd3e248cb03fb97f373e8eb8
Giannagp <giannagp@users.noreply.github.com>
2020-10-15 21:18:49 -0600

    project

--- web/projectone/view/joblist.php
+++ web/projectone/view/joblist.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/projectone/modules/head.php'; 
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/projectone/modules/head.php';
 
 ?>
 
@@ -9,34 +9,14 @@
 
     <?php
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+    $db = get_db();
 
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-	
     $statement = $db->prepare('SELECT * FROM job');
-    $statement-> execute();
-   
+    $statement->execute();
+
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
 
-        
+
         echo 'Job Name: ' . $row['jobname'];
     }
 
