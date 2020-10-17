a620108e71a673d14eed990cb6c28aa83065c972
Giannagp <giannagp@users.noreply.github.com>
2020-10-17 08:58:35 -0600

    project one'

--- web/projectone/view/categoryjob.php
+++ web/projectone/view/categoryjob.php
@@ -12,7 +12,7 @@
 
         function getcategory($categoryid){
         
-        include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
+        $db= connection();
         
         $sql = 'SELECT job.*, category.* 
                 FROM job 
@@ -23,15 +23,15 @@
 
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':categoryid', $categoryid, PDO::PARAM_INT);
-        $stmt->bindValue(':categoryname', $categoryname, PDO::PARAM_STR);
-        $stmt->bindValue(':jobid', $jobid, PDO::PARAM_INT);
-        $stmt->bindValue(':jobname', $jobname, PDO::PARAM_STR);
-        $stmt->bindValue(':jobcompany', $jobcompany, PDO::PARAM_STR);
-        $stmt->bindValue(':joblocation', $joblocation, PDO::PARAM_STR);
-        $stmt->bindValue(':jobsalary', $jobsalary, PDO::PARAM_STR);
-        $stmt->bindValue(':jorequirements', $jorequirements, PDO::PARAM_STR);
-        $stmt->bindValue(':jobresponsibilities', $jobresponsibilities, PDO::PARAM_STR);
-        $stmt->bindValue(':jobdescription', $jobdescription, PDO::PARAM_STR);
+        // $stmt->bindValue(':categoryname', $categoryname, PDO::PARAM_STR);
+        // $stmt->bindValue(':jobid', $jobid, PDO::PARAM_INT);
+        // $stmt->bindValue(':jobname', $jobname, PDO::PARAM_STR);
+        // $stmt->bindValue(':jobcompany', $jobcompany, PDO::PARAM_STR);
+        // $stmt->bindValue(':joblocation', $joblocation, PDO::PARAM_STR);
+        // $stmt->bindValue(':jobsalary', $jobsalary, PDO::PARAM_STR);
+        // $stmt->bindValue(':jorequirements', $jorequirements, PDO::PARAM_STR);
+        // $stmt->bindValue(':jobresponsibilities', $jobresponsibilities, PDO::PARAM_STR);
+        // $stmt->bindValue(':jobdescription', $jobdescription, PDO::PARAM_STR);
 
         $stmt->execute();
 
