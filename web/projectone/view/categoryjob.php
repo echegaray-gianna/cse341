5995ebdd8b4df78d94c59b472674307b621596a5
Giannagp <giannagp@users.noreply.github.com>
2020-10-17 08:54:22 -0600

    project one'

--- web/projectone/view/categoryjob.php
+++ web/projectone/view/categoryjob.php
@@ -8,8 +8,12 @@
 
         include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
         
-        $categoryid= $GET['id'];
+        $categoryid= filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
+        function getcategory($categoryid){
+        
+        include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
+        
         $sql = 'SELECT job.*, category.* 
                 FROM job 
                 JOIN category
@@ -18,21 +22,28 @@
                 
 
         $stmt = $db->prepare($sql);
-        $stmt->bindValue(':categoryid', $categoryid);
-        $stmt->bindValue(':categoryname', $categoryname);
-        $stmt->bindValue(':jobid', $jobid);
-        $stmt->bindValue(':jobname', $jobname);
-        $stmt->bindValue(':jobcompany', $jobcompany);
-        $stmt->bindValue(':joblocation', $joblocation);
-        $stmt->bindValue(':jobsalary', $jobsalary);
-        $stmt->bindValue(':jorequirements', $jorequirements);
-        $stmt->bindValue(':jobresponsibilities', $jobresponsibilities);
-        $stmt->bindValue(':jobdescription', $jobdescription);
+        $stmt->bindValue(':categoryid', $categoryid, PDO::PARAM_INT);
+        $stmt->bindValue(':categoryname', $categoryname, PDO::PARAM_STR);
+        $stmt->bindValue(':jobid', $jobid, PDO::PARAM_INT);
+        $stmt->bindValue(':jobname', $jobname, PDO::PARAM_STR);
+        $stmt->bindValue(':jobcompany', $jobcompany, PDO::PARAM_STR);
+        $stmt->bindValue(':joblocation', $joblocation, PDO::PARAM_STR);
+        $stmt->bindValue(':jobsalary', $jobsalary, PDO::PARAM_STR);
+        $stmt->bindValue(':jorequirements', $jorequirements, PDO::PARAM_STR);
+        $stmt->bindValue(':jobresponsibilities', $jobresponsibilities, PDO::PARAM_STR);
+        $stmt->bindValue(':jobdescription', $jobdescription, PDO::PARAM_STR);
 
         $stmt->execute();
 
         $jobinfo = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        return $jobinfo;
+
+        }
+
+        $jobinfo = getcategory($categoryid);
+
+        
             print_r($jobinfo);
             
             $jobname= $jobinfo['jobname'];
