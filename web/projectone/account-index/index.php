4559d70ba6772b454cf2a15cf9744747c198f4a0
Giannagp <giannagp@users.noreply.github.com>
2020-10-17 14:01:03 -0600

    project one'

--- web/projectone/account-index/index.php
+++ web/projectone/account-index/index.php
@@ -5,11 +5,6 @@ $page_title = 'Login';
 
 switch ($action) {
 
-  case 'registration':
-    //Add Title
-    $page_title = 'Registration';
-    header('location:../view/registration.php');
-    break;
 
   default:
 
