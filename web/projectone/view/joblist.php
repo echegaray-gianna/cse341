e5fdb8f90308efdaf8ddeabe533f061815031039
Giannagp <giannagp@users.noreply.github.com>
2020-10-16 20:10:30 -0600

    project one

--- web/projectone/view/joblist.php
+++ web/projectone/view/joblist.php
@@ -5,30 +5,24 @@
 
 <main>
 
-    <h1> Job Category </h1>
-    <h3> Please select a category </h3>
-
-    <?php
-
-    include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
-    $sql = 'SELECT * FROM category';
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    while ($categories = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $catname = $categories['categoryname'];
-        $catid = $categories['categoryid'];
-    }
-   
-
-    ?>
-
-
-
+    <h1 class="title-category"> Job Category </h1>
+    <h3 class="subtitle-category"> Please select a category </h3>
 
     <div for="categoryId">
         <p>Category</p>
+
         <?php
-         echo "<h4 class= 'category-list'> $catname <a href='/projectone/view/categoryjob.php?id=$catid'> Select </a> </h4>";
+
+        include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
+        $sql = 'SELECT * FROM category';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        while ($categories = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $catname = $categories['categoryname'];
+            $catid = $categories['categoryid'];
+        }
+
+        echo "<h4 class= 'category-list'> $catname <a href='/projectone/view/categoryjob.php?id=$catid'> Select </a> </h4>";
 
         ?>
 
