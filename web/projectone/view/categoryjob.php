a7cd23c799966a57207d19158a33c0784fa9d1e9
Giannagp <giannagp@users.noreply.github.com>
2020-10-17 08:37:18 -0600

    project one'

--- web/projectone/view/categoryjob.php
+++ web/projectone/view/categoryjob.php
@@ -6,42 +6,44 @@
 <main>
     <?php
 
-    include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
-    
-    $categoryid= $GET['id'];
-
-    $sql = 'SELECT job.*, category.* 
-            FROM job 
-            JOIN category
-            ON job.categoryid = category.categoryid
-            WHERE job.categoryid = :categoryid';
-            
-
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':categoryid', $categoryid);
-    //$stmt->bindValue(':categoryname', $categoryname);
-    // $stmt->bindValue(':jobid', $jobid);
-    //$stmt->bindValue(':jobname', $jobname);
-    // $stmt->bindValue(':jobcompany', $jobcompany);
-    // $stmt->bindValue(':joblocation', $joblocation);
-    // $stmt->bindValue(':jobsalary', $jobsalary);
-    // $stmt->bindValue(':jorequirements', $jorequirements);
-    // $stmt->bindValue(':jobresponsibilities', $jobresponsibilities);
-    // $stmt->bindValue(':jobdescription', $jobdescription);
-
-    $stmt->execute();
-
-    while ($jobinfo = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
-        print_r($jobinfo);
+        include $_SERVER['DOCUMENT_ROOT'] . '/projectone/connections/jobconnection.php';
         
-        $jobname= $jobinfo['jobname'];
+        $categoryid= $GET['id'];
 
-       
-         echo "<h4 class= 'category-list'> $jobinfo</h4>";
+        $sql = 'SELECT job.*, category.* 
+                FROM job 
+                JOIN category
+                ON job.categoryid = category.categoryid
+                WHERE job.categoryid = :categoryid';
+                
 
-         echo "hola";
-    }
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':categoryid', $categoryid);
+        //$stmt->bindValue(':categoryname', $categoryname);
+        // $stmt->bindValue(':jobid', $jobid);
+        //$stmt->bindValue(':jobname', $jobname);
+        // $stmt->bindValue(':jobcompany', $jobcompany);
+        // $stmt->bindValue(':joblocation', $joblocation);
+        // $stmt->bindValue(':jobsalary', $jobsalary);
+        // $stmt->bindValue(':jorequirements', $jorequirements);
+        // $stmt->bindValue(':jobresponsibilities', $jobresponsibilities);
+        // $stmt->bindValue(':jobdescription', $jobdescription);
+
+        $stmt->execute();
+
+        while ($jobinfo = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+            print_r($jobinfo);
+            
+            $jobname= $jobinfo['jobname'];
+
+        
+            echo "<h4 class= 'category-list'> $jobinfo</h4>";
+
+            echo "hola";
+        }
+
+        echo "hola";
 
     ?>
 
