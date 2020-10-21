a37717a5c8c575c4dcac68091030f7dc33f19295
Giannagp <giannagp@users.noreply.github.com>
2020-10-21 15:34:46 -0600

    portfolio

---
+++ web/eportfolio/view/home.php
@@ -0,0 +1,39 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/eportfolio/modules/header.php'; ?>
+
+<main>
+    <h1 class="title">Home</h1>
+    <div class="container">
+
+        <div class="container-text">
+
+            <p class="intro-text one">
+                My name is Gianna. I am a wife, a mother of a 4 year old daughter, and a student. I’m originally from Cordoba, Argentina.
+                I have lived in Lehi, Utah since I got married 8 years ago.
+            </p>
+
+            <p class="intro-text two">
+                I am currently finishing my degree in Web Design Development at BYU-I. I am a creative person.
+                I try to use my creativity within web pages and home projects.
+            </p>
+
+            <p class="intro-text three">
+                I always look for a way to be motivated. This helps me be more optimistic when difficulties appear. I am originally from Argentina and something I struggle with a lot is the language.
+                I didn't want to talk because I was embarrassed. In recent years I started to see this challenge differently. Instead of letting the embarrassment get me, I started talking in English more.
+                Even though I make mistakes, I can say this has helped me a lot, not only to communicate more, but also to know how to face problems and have a more positive attitude.
+            </p>
+
+            <p class="intro-text four">
+                I like to strive in everything I do and always give more. For example, now that I am a student, when I have to do an assignment, I try to do my best to "do more." When I'm writing code and it doesn't work, I never give up.
+                Although at times it can be frustrating, I like to finish what I start. After I manage to do it, I feel very capable.
+            </p>
+
+        </div>
+
+    </div>
+</main>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/eportfolio/modules/footer.php'; ?>
+
+</body>
+
+</html>
