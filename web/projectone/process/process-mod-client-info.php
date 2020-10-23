13813d3e4d65bc3e091d6cf33d45e56e4152cf03
Giannagp <giannagp@users.noreply.github.com>
2020-10-22 18:18:14 -0600

    project one

--- web/projectone/process/process-mod-client-info.php
+++ web/projectone/process/process-mod-client-info.php
@@ -1,35 +1,30 @@
 <?php
 
-require_once "../connections/dbconnect.php";
-require_once "../functions/functions.php";
-session_start();
+// require_once "../connections/dbconnect.php";
+// require_once "../functions/functions.php";
+// session_start();
 
-$db = getdb();
+// $db = getdb();
 
-try {
-$clientInfo = getAccountInfo($clientid);
-$clientInfoAcc= $_SESSION['clientdata'];
+// try {
 
-if (isset($clientInfoAcc['clientfirstname'])) { 
-  $page_title = "Update $clientInfoAcc[clientfirstname]'s Account ";
+// $clientInfo = getAccountInfo($clientid);
+// $clientInfoAcc= $_SESSION['clientdata'];
 
-} elseif (isset($clientfirstname)) { 
-  echo $clientfirstname; 
-};
+// if (isset($clientInfoAcc['clientfirstname'])) { 
+//   $page_title = "Update $clientInfoAcc[clientfirstname]'s Account ";
 
+// } elseif (isset($clientfirstname)) { 
+//   echo $clientfirstname; 
+// };
 
-// check if the $clientInfo has any data.
-if($clientInfo ===TRUE){
-  $_SESSION ['$message'] = 'Sorry, account information could not be found.';
-}
+// include '/projectone/view/client-update.php';
+// exit;
 
-include '/projectone/view/client-update.php';
-exit;
-
-}catch (Exception $ex)
-{
-    // Please be aware that you don't want to output the Exception message in
-    // a production environment
-    echo "Error with DB. Details: $ex";
-    die();
-}
+// }catch (Exception $ex)
+// {
+//     // Please be aware that you don't want to output the Exception message in
+//     // a production environment
+//     echo "Error with DB. Details: $ex";
+//     die();
+// }
