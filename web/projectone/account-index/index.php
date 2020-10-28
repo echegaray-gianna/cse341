fffd8c49f25e194db74ac982f182ea4077051053
Giannagp <giannagp@users.noreply.github.com>
2020-10-28 14:23:06 -0600

    project one

--- web/projectone/account-index/index.php
+++ web/projectone/account-index/index.php
@@ -3,6 +3,11 @@
 
 session_start();
 
+$clientfirstname = $_SESSION['clientdata']['clientfirstname'];
+$clientlastname = $_SESSION['clientdata']['clientlastname'];
+$clientemail = $_SESSION['clientdata']['clientemail'];
+$clienttype = $_SESSION['clientdata']['clienttype'];
+
 $page_title = 'Login';
 
 switch ($action) {
