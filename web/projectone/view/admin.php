8846065fbf170dd9cdeb748bc28f4f5124042e9f
Giannagp <giannagp@users.noreply.github.com>
2020-10-28 14:53:47 -0600

    project one

--- web/projectone/view/admin.php
+++ web/projectone/view/admin.php
@@ -14,7 +14,7 @@ session_start();
     $clientlastname = $_SESSION['clientdata']['clientlastname'];
     $clientemail = $_SESSION['clientdata']['clientemail'];
     $clienttype = $_SESSION['clientdata']['clienttype'];
-    
+    $clientid = $_SESSION['clientdata']['clientid'];
 
 
     echo "<h1> $clientfirstname $clientlastname </h1>
