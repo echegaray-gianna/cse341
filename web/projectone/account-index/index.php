9163b71368d00aebeb12e267156c4455f8a249f6
Giannagp <giannagp@users.noreply.github.com>
2020-10-30 11:40:57 -0600

    project one

--- web/projectone/account-index/index.php
+++ web/projectone/account-index/index.php
@@ -10,6 +10,11 @@ $page_title = 'Login';
 switch ($action) {
 
   default:
+  $clientfirstname = $_SESSION['clientdata']['clientfirstname'];
+  $clientlastname = $_SESSION['clientdata']['clientlastname'];
+  $clientemail = $_SESSION['clientdata']['clientemail'];
+  $clienttype = $_SESSION['clientdata']['clienttype'];
+  $clientid = $_SESSION['clientdata']['clientid'];
 
 
     include '../view/admin.php';
